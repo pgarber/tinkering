ecfcbf1d73fc30b1b68670c6ed64a934cb7e7d76
John Matthews <jmatthews@redhat.com>
2012-12-12 09:29:24 -0500

    Add timing info

--- yum/parse_metadata.py
+++ yum/parse_metadata.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
 
 import datetime
+import os
 import yum
 import sys
 import tempfile
+import time
 from optparse import OptionParser
 
 def create_yum_repo(label, repo_url, repo_dir=None):
@@ -28,25 +30,31 @@ def get_package_list(repo, newest=False):
         return sack.returnNewestByNameArch()
     return sack.returnPackages()
 
-
 if __name__ == "__main__":
     parser = OptionParser(description="Tool to download yum repo metadata and display # of available packages")
     parser.add_option("--url", action="store", help="URL of source repository", default=None)
-    parser.add_option("--dir", action="store", help="Directory to store metadata in", default=None)
-    parser.add_option("--label", action="store", help="Label", default="yum_repo_metadata_%s" % (datetime.datetime.now().strftime("%d_%m_%y_%H%M")))
+    parser.add_option("--dir", action="store", help="Directory to store metadata in", default="./output")
+    parser.add_option("--label", action="store", help="Label", default="yum_repo_metadata_%s" % (datetime.datetime.now().strftime("%d_%m_%y_%H%M_%S")))
     (opts, args) = parser.parse_args()
-
+    
     label = opts.label
     repo_url = opts.url
+    repo_dir = opts.dir
     if not repo_url:
         print "Please re-run with --url"
         sys.exit(1)
-    repo_dir = opts.dir
-    if not repo_dir:
-        repo_dir = "./"
-    repo = create_yum_repo(label, repo_url, repo_dir)
-    process_metadata(repo)
-    packages = get_package_list(repo)
 
-    print "YUM Repository Metadata for %s has been saved at: %s" % (repo_url, repo_dir)
+    if not os.path.exists(repo_dir):
+        os.makedirs(repo_dir)
+    repo = create_yum_repo(label, repo_url, repo_dir)
+    print "Begin download of metadata from: %s" % (repo_url)
+    a = time.time()
+    process_metadata(repo)
+    b = time.time()
+    packages = get_package_list(repo)
+    c = time.time()
+    print "%s seconds to download metadata" % (b - a)
+    print "%s seconds to parse metadata" % (c - b)
+    print "%s total time" % (c - a)
+    print "YUM Repository Metadata for %s has been saved at: %s" % (repo_url, os.path.join(repo_dir, label))
     print "%s packages are available" % (len(packages))
