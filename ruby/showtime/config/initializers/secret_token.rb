1b417b7188b101c2ccdd62cbf0b675758b14bdbf
John Matthews <jwmatthews@gmail.com>
2013-06-25 17:47:54 -0400

    First steps of 'showtime' from RSpec book

---
+++ ruby/showtime/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Showtime::Application.config.secret_token = 'bf896aa75b79efb5e9d1844635e37cc4d0fb3a2f5d42396aaf05119f15b7f9cfa5d3a365e510eb6b1aea4f343f03ba6eee4c9177f40268770f3fe83fd327b84e'
